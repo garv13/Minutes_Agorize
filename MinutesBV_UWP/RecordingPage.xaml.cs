e9d60be33675eb0f98c852d9721abfc476ef47c0
garv13 <garvjasuja@outlook.com>
2017-07-26 00:17:00 +0530

    3 pages ui complete

---
+++ MinutesBV_UWP/RecordingPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MinutesBV_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class RecordingPage : Page
+    {
+        public RecordingPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void RecordingIcon_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ContentDialog locationPromptDialog = new ContentDialog
+            {
+                Title = "Are You Sure?",
+                Content = "Do you want to end the recording",
+                CloseButtonText = "Yes",
+                PrimaryButtonText = "No"
+            };
+
+            ContentDialogResult result = await locationPromptDialog.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                // sont stop the recording and countinue 
+            }
+            else
+            {
+                Frame.Navigate(typeof(EnterSpeakerPage));
+            }
+        }
+    }
+}
